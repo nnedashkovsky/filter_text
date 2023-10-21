3748a1b845110edb5742da6ab7bf0a791dfba9af
nnedashkovsky <47478750+nnedashkovsky@users.noreply.github.com>
2023-10-22 01:34:03 +0300

    Update filtering.js
    теперь если даже часть букв есть в слове, оно выгрузится

--- filtering.js
+++ filtering.js
@@ -2,9 +2,9 @@ function filterText(text, selectedLetters) {
     const words = text.split(/\s+/);
     const wordCounts = new Map();
 
-    // Подсчет повторений слов
     words.forEach(word => {
-        if (selectedLetters.every(letter => word.includes(letter)) && word.split('').every(letter => selectedLetters.includes(letter))) {
+        const wordLetters = word.split('');
+        if (wordLetters.every(letter => selectedLetters.includes(letter))) {
             if (wordCounts.has(word)) {
                 wordCounts.set(word, wordCounts.get(word) + 1);
             } else {
@@ -13,12 +13,16 @@ function filterText(text, selectedLetters) {
         }
     });
 
-    // Сортировка слов по количеству повторений
-    const sortedWords = [...wordCounts.entries()].sort((a, b) => b[1] - a[1]);
+    // Создание массива с уникальными словами и их частотой
+    const uniqueWordsWithCounts = [...wordCounts.entries()];
 
-    const filteredWords = sortedWords.map(entry => entry[0]);
+    // Сортировка по частоте появления
+    uniqueWordsWithCounts.sort((a, b) => b[1] - a[1]);
 
-    const filteredText = filteredWords.join(' ');
+    // Получение только уникальных слов
+    const uniqueWords = uniqueWordsWithCounts.map(entry => entry[0]);
+
+    const filteredText = uniqueWords.join(' ');
     document.getElementById("filteredText").textContent = filteredText;
 }
 
@@ -39,7 +43,6 @@ document.getElementById("fileInput").addEventListener("change", function (event)
 });
 
 document.getElementById("filterButton").addEventListener("click", function() {
-    // Здесь добавим код для фильтрации текста из текстового поля
     const inputText = document.getElementById("inputText").value;
     const inputLetters = document.getElementById("inputLetters").value;
     const selectedLetters = inputLetters.split('');
